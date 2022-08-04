24cbf8ef889e1544b6d175f895ba026dad64fb06
diusdi <riadidion@gmail.com>
2022-08-04 21:34:31 +0700

    menambahkan tugas 1

---
+++ tugas-1_1.php
@@ -0,0 +1,27 @@
+<?php
+
+$menu1 = "Ayam Goreng";
+$menu2 = "Ayam Bakar";
+$menu3 = "Ayam Sayur";
+$menu4 = "Tempe Goreng";
+$menu5 = "Tahu Goreng";
+$menu6 = "Soto";
+$menu7 = "Nasi Putih";
+$menu8 = "Es Teh";
+$menu9 = "Es Tebu";
+
+echo "<h1> Daftar Menu </h1>";
+// echo "<br>";
+
+echo (
+    "<ul>
+        <li>$menu1 = Rp. 13000</li>
+        <li>$menu2 = Rp. 15000</li>
+        <li>$menu3 = Rp. 13000</li>
+        <li>$menu4 = Rp. 1000</li>
+        <li>$menu5 = Rp. 1000</li>
+        <li>$menu6 = Rp. 3000</li>
+        <li>$menu7 = Rp. 5000</li>
+        <li>$menu8 = Rp. 4000</li>
+        <li>$menu9 = Rp. 5000</li>
+    </ul>");
---
+++ tugas-1_2.php
@@ -0,0 +1,46 @@
+<?php
+
+$menu1 = "Cireng";
+$menu2 = "Es Duren";
+$menu3 = "Es Milo";
+$menu4 = "Es Teh";
+$menu5 = "Piscok";
+
+echo "<h1>Daftar Menu</h1>";
+
+echo (
+    "
+    <table border = 1>
+        <tr>
+            <td>No</td>
+            <td>Nama Menu</td>
+            <td>Harga</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>$menu1</td>
+            <td>Rp 1000</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>$menu2</td>
+            <td>Rp 15000</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>$menu3</td>
+            <td>Rp 20000</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>$menu4</td>
+            <td>Rp 5000</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>$menu5</td>
+            <td>Rp 1000</td>
+        </tr>
+
+    </table>
+    ");
